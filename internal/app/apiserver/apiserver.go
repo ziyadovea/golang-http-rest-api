3530d0ea3e9d8359139f57e29b5b3d9e21430d69
ziyadovea <ziyadovea@gmail.com>
2022-01-06 03:01:51 +0300

    add test

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,55 @@
 package apiserver
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
+
 // APIServer - структура для сервера
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New создает экземпляр сервера
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
 // Start запускает сервер
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+	s.configureRouter()
+	s.logger.Info("starting api server")
+	return http.ListenAndServe(s.config.Port, s.router)
+}
+
+// configureLogger задает конфигурацию для логгера
+func (s *APIServer) configureLogger() error {
+	lvl, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(lvl)
 	return nil
+}
+
+// configureRouter задает конфигурацию для роутера
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello!")
+	}
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,10 +1,15 @@
 package apiserver
 
+// Config - структура для конфига приложения
 type Config struct {
-	Port string `json:"port"`
+	Port     string `json:"port"`
+	LogLevel string `json:"log_level"`
 }
 
 // NewConfig создает новый конфиг
 func NewConfig() *Config {
-	return &Config{}
+	return &Config{
+		Port:     ":8080",
+		LogLevel: "debug",
+	}
 }
